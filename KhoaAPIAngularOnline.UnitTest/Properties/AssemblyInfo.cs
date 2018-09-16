485ee54d11e35c4ed860da9509b574b3613e83c2
DESKTOP-Q161EUF\toan2 <toan210597ntu@gmail.com>
2018-09-16 11:21:02 +0700

    the first commint

---
+++ KhoaAPIAngularOnline.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KhoaAPIAngularOnline.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("KhoaAPIAngularOnline.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("f37dd92f-de2b-45b9-8d59-38774bcf865d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ KhoaAPIAngularOnline.WebAPP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="KhoaAPIAngularOnline.WebAPP.MvcApplication" Language="C#" %>
